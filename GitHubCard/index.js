94474f38983911c25a229327578e4f3ec07ee8f2
Corbin Robb <corbinrobb@gmail.com>
2020-03-29 10:48:00 -0600

    Finish the component and add cards to DOM

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,11 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/corbinrobb')
+  .then( response => {
+    document.querySelector('.cards').appendChild(createCard(response.data));
+  });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +29,14 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then(response => {
+      document.querySelector('.cards').appendChild(createCard(response.data));
+    });
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +57,54 @@ const followersArray = [];
 </div>
 
 */
+
+function createCard(obj) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const img = document.createElement('img');
+  img.src = obj.avatar_url;
+  card.appendChild(img);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+
+  const name = document.createElement('h3');
+  name.classList.add('name');
+  name.textContent = obj.name;
+  cardInfo.appendChild(name);
+
+  const username = document.createElement('p');
+  username.classList.add('username');
+  username.textContent = obj.login;
+  cardInfo.appendChild(username);
+
+  const location = document.createElement('p');
+  location.textContent = `Location: ${obj.location}`;
+  cardInfo.appendChild(location);
+
+  const profile = document.createElement('p');
+  profile.innerHTML = `Profile:  
+      <a href=${obj.html_url}>${obj.name}'s github</a>`;
+  cardInfo.appendChild(profile);
+
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${obj.followers}`;
+  cardInfo.appendChild(followers);
+
+  const following = document.createElement('p');
+  following.textContent = `Following: ${obj.following}`;
+  cardInfo.appendChild(following);
+
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${obj.bio}`;
+  cardInfo.appendChild(bio);
+
+  card.appendChild(cardInfo);
+
+  return card;
+}
+
 
 /* List of LS Instructors Github username's: 
   tetondan
